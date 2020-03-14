1d396a9b27c947ed24fc13278d894a183069bc4c
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-03-13 23:56:57 -0700

    wordsearch py

--- ByProbability/_A/Graph/wordsearch/wordsearch.js
+++ ByProbability/_A/Graph/wordsearch/wordsearch.js
@@ -10,17 +10,20 @@
    
    //path is current location
    const verify = (row, col, matrix, path) => {
+     //if we are out of the bounds of the grid, or if the current element does not have the current built up word at the path location
     if(row<0 || col<0 || row>=matrix.length || col>=matrix[0].length || matrix[row][col]!=word[path]
-      || path>wordLength) return false;
+      || path>wordLength) return false; //not in the search, base case, game over
+      //otherwise we increment the path location
       path++;
-      matrix[row][col]='#';
-
+      matrix[row][col]='#'; //path is the buildup element char at index of word
+      //fill out if already visited for this context
       //if we find the word
+      //that chars match and the path is complete
       if(path === wordLength) return true;
 
       //up
       if(verify(row-1, col, matrix, path)) return true;
-      //right
+      //right, recursively search the edges
       if(verify(row, col+1, matrix, path)) return true;
       //down
       if(verify(row+1, col, matrix, path)) return true;
--- ByProbability/_A/Graph/wordsearch/wordsearch.py
+++ ByProbability/_A/Graph/wordsearch/wordsearch.py
@@ -26,3 +26,42 @@ board and word consists only of lowercase and uppercase English letters.
 1 <= word.length <= 10^3
 """
 # key ideas: Back track, depth first search
+#python3 static typing
+class Solution:
+  def exist(self, board: List[List[str]], word: str) -> bool:
+    
+    #recursive function, r, c are the positions on the board location
+    def recur(word, r, c):
+      #if characters don't match
+      if word[0] !=board[r][c]:
+        return False
+      if len(word) == 1:
+        return True
+        #base case
+      
+      ans =False
+      #default
+      #add the current position on grid to used, pairs 
+      used.add((r,c))
+      #check for previous positions surrounding to traverse/not checked/expanded yet
+      if r > 0 and (r-1, c) not in used:
+        #bitwise or, get the recusive call of the substring word remaining
+        ans |= recur(word[1:], r-1, c)
+      if c > 0 and (r, c-1) not in used and not ans:
+        ans |= recur(word[1:],r, c-1)
+      #within bounds of the board
+      if r < len(board)-1 and (r+1, c) not in used and not ans:
+        ans |= recur(word[1:],r+1,c)
+      if c < len(board[0])-1 and (r, c+1) not in used and not ans:
+        ans |= recur(word[1:], r, c+1)
+      used.remove((r, c))
+      return ans
+
+    used = set()
+    for r in range(len(board)):
+      for c in range(len(board[0])):
+        if recur(word, r, c):
+          # //for starting position in the board
+          return True
+
+    return False
