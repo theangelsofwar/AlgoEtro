0a1c8f40237d5e67d5a7f379f4dc270542720132
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-03-13 20:36:07 -0700

    mergePy

--- Data Structures/LinkedList/MergeKSortedLists/mergeKList.py
+++ Data Structures/LinkedList/MergeKSortedLists/mergeKList.py
@@ -24,6 +24,46 @@ class Solution(object):
 """
 
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if not lists: return 
+        n = len(lists)
+        if n == 1: return lists[0]
+        mid = n//2
+        left = self.mergeKLists(lists[:mid])
+        # print(left)
+        right = self.mergeKLists(lists[mid:])
+                
+        return self.merge(left, right)
+    
+    def merge(self, left, right):
+        dummy = ListNode(0)     
+        cur = dummy
+        while left and right:
+            if left.val <=right.val:
+                cur.next = left
+                left = left.next
+            else:
+                cur.next = right
+                right = right.next
+            cur=cur.next
+            
+        if left:
+            cur.next = left
+        if right:
+            cur.next = right
+        return dummy.next
+
 """ Solution 2
 Merge with Divide and Conquer
 O nlogk time, O(1) space
--- Data Structures/LinkedList/MergeKSortedLists/mergeKLists.js
+++ Data Structures/LinkedList/MergeKSortedLists/mergeKLists.js
@@ -12,3 +12,72 @@ Input:
 ]
 Output: 1->1->2->3->4->4->5->6 
 */
+
+//Option 1
+function mergeLists(a, b){
+  const dummy = new ListNode(0);
+  let temp = dummy;
+  while(a !== null && b !== null) {
+    if(a.val < b.val){
+      temp.next = a;
+      a = a.next;
+    } else {
+      temp.next = b;
+      b = b.next;
+    }
+    temp = temp.next;
+  }
+  if(a !== null){
+    temp.next = a;
+  }
+  if( b!== null){
+    temp.next = b;
+  }
+  return dummy.next;
+}
+
+var mergeKLists = function(lists){
+  if(lists.length === 0){
+    return null;
+  }
+
+  while(lists.length > 1){
+    let a = lists.shift();
+    let b = lists.shift();
+    const h = mergeLists(a, b);
+    lists.push(h);
+  }
+
+  return lists[0];
+};
+
+//second, divide and conquer
+function merge(left, right){
+  if(!left){
+    return right;
+  } else if(!right){
+    return left;
+  } else if(left.val < right.val){
+    left.next = merge(left.next, right);
+    return left;
+  } else {
+    right.next = merge(left, right.next);
+    return right;
+  }
+}
+
+function helper(lists, start, end) {
+  if(start === end) {
+    return lists[start];
+  } else if (start < end) {
+    const mid = parseInt((start+end)/2);
+    const left = helper(lists, start, mid);
+    const right = helper(lists, mid+1, end);
+    return merge(left, right);
+  } else {
+    return null;
+  }
+}
+var mergeKLists = function(lists){
+  return helper(lists, 0, lists.length-1);
+};
