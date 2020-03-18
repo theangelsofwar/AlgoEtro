d9e0c2f6ffe72981b162ee22fcb3f1ae8c0ee2c1
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-03-17 17:52:12 -0700

    economy

--- DynamicProgramming/knapsack/knapsack2.js
+++ DynamicProgramming/knapsack/knapsack2.js
@@ -23,10 +23,72 @@
 // Output: false
 
 // Explanation: The array cannot be partitioned into equal sum subsets.
+// solution 1 72 ms 35.2 MB
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 const canPartition = (nums )=> {
+  let sum = 0;
+  for(let i =0; i< nums.length; i++){
+    sum += nums[i];
+  }
 
-}
+  if(sum % 2 !== 0) return false; //cannot divide ints evenly
+  sum/=2; //inversion
+  let dp = new Array(sum+1).fill(false);
+  dp[0] = true; //first element is true since this is dummy position
+
+  for(let i = 0; i<nums.length; i++){
+    let num = nums[i];
+    for(let j = sum; j>=num; j--){
+      dp[j] = dp[j] || dp[j-num];//if already true, short circuit to TRUE || FALSE = TRUE
+    }
+  }
+  return dp[sum];
+};
+
+
+//solution 2 uses reduce 88ms 35.4MB
+//calling reduce adds layer of function complexity and a slight lag time, the tradeoff for modularity and not having to re-create
+const canPartition = (nums) => {
+  const sum = nums.reduce((sum, currNum) => sum+=currNum, 0); //sum
+  if(!sum%2) return false; //must be even
+
+  const target = sum/2; //target halve the sum
+  const cache = new Array(target+1).fill(false);
+  cache[0] = true;
+
+  for(let i =0; i<nums.length; i++){
+    let num = nums[i];
+    for(let k=target; k>=num; k--){
+      cache[k] = cache[k] || cache[k-num];
+    }
+  }
+  return cache[target];
+};
+
+
+// third, 80ms , 35.6MB very similar
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const canPartition = (nums) => {
+  let sum = 0; 
+  for(let i =0; i<nums.length; i++){
+    sum+=num;
+  }
+
+  if(sum%2!=0) return false;
+  let half=sum/2;
+  let dp=new Array(half+1).fill(false);
+  dp[0]=true; //dummy placeholder
+  for(let i=0; i<nums.length;i++){
+    let num = nums[i];
+    for(let k=0; k>=num; k--){
+      dp[k]= dp[k] || dp[k-num];
+    }
+  }
+  return dp[half];
+};
