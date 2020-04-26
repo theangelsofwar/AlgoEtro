62a5740cf006b2e32ad7cd7515f6b842d674a03a
Angie <angiechangpagne@gmail.com>
2020-04-25 18:30:12 -0700

    dikjstra

---
+++ shortestPath.js
@@ -0,0 +1,22 @@
+const Node = function(value){
+  this.value = value;
+  this.next = null;
+};
+
+/**
+ * 
+ * @param {*} to Node
+ * @param {*} from Node
+ * @return Linked List Path of the Nodes
+ */
+const shortestPath = (to, from) => {
+  if(!a || a === null || !b || b === null) {
+    return;
+  }
+  else if(a === b) {
+    return null; //the path is none
+  }
+
+};
+
+
