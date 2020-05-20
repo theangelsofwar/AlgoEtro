8a34d43f7c7194bde50b6b404210787bdbe73093
Angie <angiechangpagne@gmail.com>
2020-05-19 19:13:12 -0700

    ships

--- Leet1011/Leet1011.js
+++ Leet1011/Leet1011.js
@@ -38,13 +38,7 @@ Explanation:
 1st day: 1
 2nd day: 2
 3rd day: 3
-4th day: 1, 1
- 
-
-Note:
-
-1 <= D <= weights.length <= 50000
-1 <= weights[i] <= 500} weights 
+4th day: 1, 1} weights 
  * @param {*} D 
  */
 /**
@@ -53,54 +47,78 @@ Note:
  * @return {number}
  */
 var shipWithinDays = function(weights, D) {
-  //input check
-  if(!weights || !D || D<0) return -1;
-  //see my diagram in my blog...
-  //sliding windows spliced....
-  //array splice method recursive base case solve..
-  
-  
-  //revese of stock price buy sell or knapsack or stairs...
-  //essentially, this is like a reverse average, so we want to split the intervals approximately evenly by their sum..
-  
-  //the brute force way is to get the sum, get the average weight capacity, 
-  
-  //best way is to graph on x and y axis, x axis is time Day t=0..D with D Days as the block constraint, and y axis is the sum of the set of graphed weights that can be pushed to the prior day, taken for the current day, or saved for the next day within the constraint of D Days,
-  //D days is a block constraint, while the y axis for Capacity on a given day can vary
-  //send back the capacity
-  
-  let c = 0;
-  
-  //on a given day, take from weights[i] up until (including) weights[i+k]... and track a global max, 
-  
-  //to optimize the spread we want want the Capacity c to be > largest element of the array(depending on the range of positives), and with the least additions based on the spread range of D days, to do this we can grab the average
-  let avg = weights.reduce((a,b) => a+b, 0)/weights.length;
-  //the issue with avg is outliers, eg 1 extreme weight, in which case that would be usually the capacity and taken as 1 item on the trip, distributing the rest, so we could compare median to mean...
-  //we do not need to necessary know the median, 
-  let localCapacity = 0;
-  //try to avoid iterative through the weights array multiple times, track that index stored and the capacity Math.max
-  let index = 0; 
-  for(let t=0; t<D; t++){
-      let el = weights[index]; //element enxt on the queue
-      if(localCapacity+el<avg){
-          //take it, and we stay on the current day, therefore
-          t--; //stay on the current day
-          localCapacity+=el; //accumulation of the account...
-          index++; //rise of the indexed fund
-      }
-      else{
-          //leave it on the iteration day t of D days, and empty localCapacity
-          c = Math.max(localCapacity, c);
-          localCapacity = 0;
-          //day will increment and continue;
-      }
-      //do one check to compare localCapacity
-      c=Math.max(c, localCapacity);
-      
-      //we can do a take it or leave it, with recursive backtracking, but this is less efficient...
-      //what are we optimizing? what are the constraints?
-      //getting the minimum capacity by spreading it evenly through D days, with unequal, uncertain amounts
-  }
-  
-  return c;
+    //input check
+    if(!weights || !D || D<0) return -1;
+    //see my diagram in my blog...
+    //sliding windows spliced....
+    //array splice method recursive base case solve..
+    
+    
+    //revese of stock price buy sell or knapsack or stairs...
+    //essentially, this is like a reverse average, so we want to split the intervals approximately evenly by their sum..
+    
+    //the brute force way is to get the sum, get the average weight capacity, 
+    
+    //best way is to graph on x and y axis, x axis is time Day t=0..D with D Days as the block constraint, and y axis is the sum of the set of graphed weights that can be pushed to the prior day, taken for the current day, or saved for the next day within the constraint of D Days,
+    //D days is a block constraint, while the y axis for Capacity on a given day can vary
+    //send back the capacity
+    
+    var c; //c is the discrete minimum between the average Capacity per the day over D days, so the next discrete level up (all positive ints)
+    
+    //on a given day, take from weights[i] up until (including) weights[i+k]... and track a global max, 
+    
+    //to optimize the spread we want want the Capacity c to be > largest element of the array(depending on the range of positives), and with the least additions based on the spread range of D days, to do this we can grab the average
+    var wacc = Math.floor(weights.reduce((a,b) => a+b, 0)/D);
+    //we want to take the working average of the total Absolute Capacity of the boxed elements across D days, a,d we must surpass it by a little discrete amount or meet it exactly on a given day t=i....D Days.
+    //a weighted average
+    //the issue with avg is outliers, eg 1 extreme weight, in which case that would be usually the capacity and taken as 1 item on the trip, distributing the rest, so we could compare median to mean...
+    //we do not need to necessary know the median, 
+    c = wacc; //initialize it to wacc...because you will see...
+    
+    var localCapacity = weights[0]; //start out
+    //try to avoid iterative through the weights array multiple times, track that index stored and the capacity Math.max
+    let index = 1; 
+    for(let t=0; t<D && index<weights.length; t++){
+        //bi conditional, based on D days but constrained by array of weights as well
+        let el = weights[index]; //element enxt on the queue
+        //p = D-t days left to complete everything...
+        //we must factor in the days left as part of a weighted avg
+        //must factor remaining days as well as the average across because discrete logic.
+        if(localCapacity+el<wacc) {
+            //take it, and we stay on the current day, therefore
+            localCapacity+=el; //accumulation of the account...
+            index++; //rise of the indexed fund on weights
+            t--; //stay on the current day, offset t++
+
+        }
+        else {
+            //leave it on the iteration day t of D days, and empty localCapacity
+            //do another check first, if unassigned, since wacc is a non discrete number, but our result must be discreet, do not leave room for coicidence...
+            //we must check again based on not the wacc, but now based on what we have saved to c in the store
+            //we might have to move back an index-- this time while moving forward a day t++
+            // take out && c===wacc because that is probable on coincidence
+            if(localCapacity+el>=c && t===0){
+                //this is a maximization at the same time it is a minimization, we want to output the minimum weight per day to meet D days requirements, but we also must keep track of the maximum of the days to fulfill the shipping requirements in our trade war with China. 
+                //if it is greater than, we can leave it for the next day, because it is still created than wacc, which means the next discrete number day has enough days left...
+                localCapacity += el;
+                index++; //go back a day for next capacity, leave it
+                c = Math.max(localCapacity, c);
+
+            }
+            else {
+                c = Math.max(localCapacity, c);
+                index--;
+                localCapacity = el; //next element for the next day with incrementing t, 
+            }
+            //day will increment and continue;t++ into the next day
+        }
+    }
+    //do one check to compare localCapacity
+        // c= Math.max(c, localCapacity);
+        
+        //we can do a take it or leave it, with recursive backtracking, but this is less efficient...
+        //what are we optimizing? what are the constraints?
+        //getting the minimum capacity by spreading it evenly through D days, with unequal, uncertain amounts
+    //graph it out! :D
+    return c;
 };
