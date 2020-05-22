f78127949d122cc19c8efed8b9c772d162aac7d2
Angie <angiechangpagne@gmail.com>
2020-05-21 19:10:00 -0700

    area51

--- Leet695.js
+++ Leet695.js
@@ -24,44 +24,44 @@ Note: The length of each dimension in the given grid does not exceed 50.
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-  //DFS, use a queue
-  let result = 0;
-  //mutate grid to zeros to not repeat
-  for(let i=0; i<grid.length; i++){
-      //iter rows
-      for(let j=0; j<grid[0].length; j++) {
-          //iter cols
-          // let el = grid[i][j];
-          let area51 = processIsland(grid, i, j);
-          if(area51>=4){
-              result = Math.max(area51,result);
-          }
-      }
-  }
-  
-  return result;
+    //DFS, use a queue
+    let result = 0;
+    //mutate grid to zeros to not repeat
+    for(let i=0; i<grid.length; i++){
+        //iter rows
+        for(let j=0; j<grid[0].length; j++) {
+            //iter cols
+            // let el = grid[i][j];
+            let area51 = processIsland(grid, i, j);
+            
+                result = Math.max(area51,result);
+            
+        }
+    }
+    
+    return result;
 };
 
 
 //if count > 4 vertical or horizontally, DFC then we can return true
 var processIsland = function(grid, i, j) {
-  let area51 = 0;
-  
-  
-  if(!grid || grid.length===0 || i<0 || j<0 || i>=grid.length || j>=grid[0].length){
-      return 0; //0 is also false
-  }
-  else{
-      let el = grid[i][j];
-      if(el===1){
-          area51++;
-          grid[i][j]=0;
-          area51+=processIsland(grid, i+1,j);
-          area51+=processIsland(grid, i-1,j);
-          area51+=processIsland(grid, i,j+1);
-          area51+=processIsland(grid, i, j-1); //not diagonals, i-1 j-1, 
-      }
-      
-  }
-  return area51;
+    let area51 = 0;
+    
+    
+    if(!grid || grid.length===0 || i<0 || j<0 || i>=grid.length || j>=grid[0].length){
+        return 0; //0 is also false
+    }
+    else{
+        let el = grid[i][j];
+        if(el===1){
+            area51++;
+            grid[i][j]=0;
+            area51+=processIsland(grid, i+1,j);
+            area51+=processIsland(grid, i-1,j);
+            area51+=processIsland(grid, i,j+1);
+            area51+=processIsland(grid, i, j-1); //not diagonals, i-1 j-1, 
+        }
+        
+    }
+    return area51;
 }
